d807cc82c02d524714a676811f52d7897b5bd6d6
ChoiSeongmuk <sawdonnie@icloud.com>
2023-06-30 16:20:40 +0900

    Feature: init page setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Review from "./pages/reveiw";
 
 function App() {
-  return <div>HI</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={"/"} element={<Review />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
